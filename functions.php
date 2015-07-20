53dc2f50bb6a394f85d3d6fa19fd651fb0349c3e
Devin Price <devin@wptheming.com>
2015-07-20 18:23:22 -0500

    Trac 4297 Remove HTML5 support from functions.php.

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ function gather_setup() {
 	 * to output valid HTML5.
 	 */
 	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+		'comment-list', 'gallery', 'caption'
 	) );
 
 	// Post editor styles
