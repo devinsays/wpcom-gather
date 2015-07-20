59d5a052bdc9b12734e4a573651498d05cd74e65
Devin Price <devin@wptheming.com>
2015-07-20 18:40:40 -0500

    Trac 4295. Remove .pot file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,44 +32,6 @@ module.exports = function(grunt) {
 				dest: 'style.css'
 			}
 		},
-	    // https://www.npmjs.org/package/grunt-wp-i18n
-	    makepot: {
-	        target: {
-	            options: {
-	                domainPath: '/languages/',
-	                potFilename: 'gather.pot',
-	                potHeaders: {
-	                poedit: true, // Includes common Poedit headers.
-                    'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
-                },
-		        type: 'wp-theme',
-		        updateTimestamp: false,
-		        processPot: function( pot, options ) {
-					pot.headers['report-msgid-bugs-to'] = 'https://devpress.com/';
-		        	pot.headers['language'] = 'en_US';
-		        	return pot;
-					}
-				}
-			}
-		},
-		dirs: {
-			lang: 'languages',
-		},
-		potomo: {
-			dist: {
-				options: {
-					poDel: false // Set to true if you want to erase the .po
-				},
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.lang %>',
-					src: ['*.po'],
-					dest: '<%= dirs.lang %>',
-					ext: '.mo',
-					nonull: true
-				}]
-			}
-		},
 		cssjanus: {
 			theme: {
 				options: {
@@ -116,7 +78,6 @@ module.exports = function(grunt) {
     	'replace',
     	'sass',
     	'autoprefixer',
-		'makepot',
 		'cssjanus'
 	]);
 
