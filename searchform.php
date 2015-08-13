3d02e1e4f2b1fae20e8f00fddc7616efe0c65451
Devin Price <devin@wptheming.com>
2015-08-12 22:12:43 -0500

    Updates for search form.

--- searchform.php
+++ searchform.php
@@ -6,12 +6,12 @@
  */
 ?>
 
-<form role="search" method="get" class="search-form clearfix" action="<?php echo home_url( '/' ); ?>">
+<form role="search" method="get" class="search-form clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php esc_html_e( 'Search for:', 'gather' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php esc_html_e( 'Search...', 'gather' ); ?>" value="" name="s" title="<?php esc_html_e( 'Search for:', 'gather' ); ?>" />
+		<input type="search" class="search-field" placeholder="<?php esc_html_e( 'Search&hellip;', 'gather' ); ?>" value="" name="s" title="<?php esc_html_e( 'Search for:', 'gather' ); ?>" />
 	</label>
 	<button type="submit" class="search-submit">
-		<div class="gather-icon-search"></div><span class="screen-reader-text"><?php esc_html_e( 'Search...', 'gather' ); ?></span>
+		<div class="gather-icon-search"></div><span class="screen-reader-text"><?php esc_html_e( 'Search&hellip;', 'gather' ); ?></span>
 	</button>
 </form>
